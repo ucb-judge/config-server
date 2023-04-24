e74f957065c476b9501875a39678f9bff8d405b8
iguiIllanes <igui10082002@gmail.com>
2023-04-23 21:24:07 -0400

    Initial commit

---
+++ src/main/kotlin/com/ucbjudge/configserver/config/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.ucbjudge.configserver.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun filterChain (http :HttpSecurity) : SecurityFilterChain {
+        http.csrf()
+            .ignoringAntMatchers("/encrypt/**")
+            .ignoringAntMatchers("/decrypt/**")
+        return http.build()
+    }
+}
